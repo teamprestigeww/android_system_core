be0bc0076419622f614b303c8db3e307f52299bb
cvpcs <root@cvpcs.org>
2010-12-30 01:06:10 -0600

    build a static utility logcat

--- logcat/Android.mk
+++ logcat/Android.mk
@@ -10,3 +10,22 @@ LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE:= logcat
 
 include $(BUILD_EXECUTABLE)
+
+# make a static executable
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= logcat.cpp event.logtags
+
+LOCAL_STATIC_LIBRARIES := liblog libstdc++ libc
+
+LOCAL_MODULE := utility_logcat
+LOCAL_MODULE_TAGS := eng
+LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
+LOCAL_MODULE_STEM := logcat
+
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+include $(BUILD_EXECUTABLE)
+
